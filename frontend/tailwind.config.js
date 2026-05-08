13cb00309732ad79d051fa63027934a8fcf961dc
Ariel <arielmaltisotto37@gmail.com>
2026-05-07 21:59:34 -0300

    arreglo visual

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
     darkMode: ["class"],
     content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
+    "./public/index.html",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/lib/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
   	extend: {
